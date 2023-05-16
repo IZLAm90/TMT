5c918499c558a25a47166ade29218817c2c53335
islam <isalm9090@gmail.com>
2023-05-16 09:28:38 +0300

    help first item

---
+++ app/src/main/java/com/patient/tmt/presentation/fragment/homenavigation/HomeOfNavigationFragment.kt
@@ -0,0 +1,32 @@
+package com.patient.tmt.presentation.fragment.homenavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.patient.tmt.R
+
+
+class HomeOfNavigationFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_of_navigation, container, false)
+    }
+
+
+}
