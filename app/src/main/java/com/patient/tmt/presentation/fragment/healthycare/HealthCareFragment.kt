719df50dee5bc0485c9881c48634eddb1bdaa44e
islam <isalm9090@gmail.com>
2023-06-12 01:05:45 +0300

    health care

---
+++ app/src/main/java/com/patient/tmt/presentation/fragment/healthycare/HealthCareFragment.kt
@@ -0,0 +1,14 @@
+package com.patient.tmt.presentation.fragment.healthycare
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.patient.base.BaseFragment
+import com.patient.tmt.R
+
+class HealthCareFragment : BaseFragment(R.layout.fragment_health_care) {
+    private lateinit var binding : HealthCareFragment
+
+}
