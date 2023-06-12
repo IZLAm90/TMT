616ac52353c9abe2bed8e1a311081cad7edd830f
islam <isalm9090@gmail.com>
2023-06-12 09:48:05 +0300

    health care

---
+++ app/src/main/java/com/patient/tmt/presentation/fragment/docprofile/DoctorProfileFragment.kt
@@ -0,0 +1,15 @@
+package com.patient.tmt.presentation.fragment.docprofile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.patient.base.BaseFragment
+import com.patient.tmt.R
+import com.patient.tmt.databinding.FragmentDoctorProfileBinding
+
+class DoctorProfileFragment : BaseFragment(R.layout.fragment_doctor_profile) {
+    private lateinit var binding:FragmentDoctorProfileBinding
+
+}
