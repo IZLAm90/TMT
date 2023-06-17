4bd12180b08221e91d9c63499cff1c39f282abf1
islam <isalm9090@gmail.com>
2023-06-18 01:15:39 +0300

    medical care useCare useCase

--- app/src/main/java/com/patient/tmt/presentation/fragment/healthycare/HealthCareFragment.kt
+++ app/src/main/java/com/patient/tmt/presentation/fragment/healthycare/HealthCareFragment.kt
@@ -5,10 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.patient.base.BaseFragment
+import com.patient.data.model.HealthCareModel
 import com.patient.tmt.R
 
 class HealthCareFragment : BaseFragment(R.layout.fragment_health_care) {
+    private val viewModel by viewModels<HealthyCareViewModel>()
     private lateinit var binding : HealthCareFragment
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val list : ArrayList<HealthCareModel> = arrayListOf()
+        list.add(HealthCareModel(R.drawable.controlofphobia,"السيطرة علي الرهاب"
+            ,"حالة من الخوف الشديد والمتواصل من مواقف او نشاطات معينة عند ",
+            "عدد الجلسات: 6"))
+        list.add(HealthCareModel(R.drawable.controlofphobia,"السيطرة علي الرهاب"
+            ,"حالة من الخوف الشديد والمتواصل من مواقف او نشاطات معينة عند ",
+            "عدد الجلسات: 6"))
+        list.add(HealthCareModel(R.drawable.controlofphobia,"السيطرة علي الرهاب"
+            ,"حالة من الخوف الشديد والمتواصل من مواقف او نشاطات معينة عند ",
+            "عدد الجلسات: 6"))
+        list.add(HealthCareModel(R.drawable.controlofphobia,"السيطرة علي الرهاب"
+            ,"حالة من الخوف الشديد والمتواصل من مواقف او نشاطات معينة عند ",
+            "عدد الجلسات: 6"))
+        list.add(HealthCareModel(R.drawable.controlofphobia,"السيطرة علي الرهاب"
+            ,"حالة من الخوف الشديد والمتواصل من مواقف او نشاطات معينة عند ",
+            "عدد الجلسات: 6"))
+        list.add(HealthCareModel(R.drawable.controlofphobia,"السيطرة علي الرهاب"
+            ,"حالة من الخوف الشديد والمتواصل من مواقف او نشاطات معينة عند ",
+            "عدد الجلسات: 6"))
+        list.add(HealthCareModel(R.drawable.controlofphobia,"السيطرة علي الرهاب"
+            ,"حالة من الخوف الشديد والمتواصل من مواقف او نشاطات معينة عند ",
+            "عدد الجلسات: 6"))
+        viewModel.getPsychologicalProgrammes(list)
+    }
 }
--- app/src/main/java/com/patient/tmt/presentation/fragment/healthycare/HealthyCareViewModel.kt
+++ app/src/main/java/com/patient/tmt/presentation/fragment/healthycare/HealthyCareViewModel.kt
@@ -3,17 +3,21 @@ package com.patient.tmt.presentation.fragment.healthycare
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.patient.data.model.HealthCareModel
-import com.patient.domain.MedicalUseCase
+import com.patient.data.model.MedicalSecialtyModel
 import com.patient.domain.medicalcare.MedicalCareUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 @HiltViewModel
 class HealthyCareViewModel@Inject constructor(private val useCase: MedicalCareUseCase) : ViewModel() {
-
-        private fun getPsychologicalProgrammes() {
+    private val _dataFlow = MutableSharedFlow<ArrayList<HealthCareModel>>()
+    val dataFlow = _dataFlow.asSharedFlow()
+         fun getPsychologicalProgrammes(list: ArrayList<HealthCareModel>) {
             viewModelScope.launch {
-//            useCase.getPsychologicalProgrammes(emptyArray<HealthCareModel>())
+                _dataFlow.emit(useCase.getPsychologicalProgrammes(list))
             }
         }
 
